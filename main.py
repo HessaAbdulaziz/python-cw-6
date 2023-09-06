a1e38248cb041062f63cc46cb7b1a1aee1b75555
HessaAbdulaziz <HessaAbdulaziz@users.noreply.github.com>
2023-09-06 19:35:46 +0300

    cw6
    cw6

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # write your code here
+person={
+    'name': 'Hessa',
+    'age' : 15,
+    'hobbies': ['reading','writing','coding'] 
+}
+
+print(person['name'])
+print(person['age'])
+person['age']=16
+
+print(person['age'])
+person['country']='kuwait'
+print(person)
+print(len(person))
+
+person['hobbies'].append('sleeping')
+def check_hobbies(person):
+    if len(person['hobbies'] )> 3 :
+        print('wow your amazing')
+    else:
+        print('add more hobbies :)')
+check_hobbies(person)
+
+
+
+
